23120ac8b3386cdbee123fc0712207556287fad3
tijmenvanetten <tijmenettenvan@gmail.com>
2023-05-26 20:57:51 +0200

    localise training into main script

--- src/__main__.py
+++ src/__main__.py
@@ -1,114 +1,173 @@
-from data import load_target, load_covariates
-from features.clustering import cluster_series
-from models import load_model
-from models import train_predict_global, train_predict_local
-from models import evaluate
-from visualization import plot, plot_separate
-from tqdm import tqdm
+from data import load_target, load_covariates, split_data
+from models import load_model, evaluate
+from visualization import plot_separate
+from utils import allow_kwargs
+from features.clustering import stack_timeseries
 
+from darts import concatenate
+from darts.dataprocessing.transformers.scaler import Scaler
+
+from sklearn.preprocessing import MinMaxScaler
 import argparse
+import logging
 import os
 from typing import Dict
-
 from datetime import datetime
 
 
-def run(config: Dict) -> Dict:
+def run(timeseries, covariates, args) -> Dict:
     """
     runs experiment
 
     """
-    # Load Data
-    target_series = load_target('data/03_processed/on_forecourt_sessions.csv', group_cols='location_id',
-                                time_col='date', value_cols='energy_delivered_kwh', static_cols=['num_evse'], freq='D')
-    covariates = load_covariates('data/03_processed/weather_ecad.csv', time_col='date',
-                                 value_cols=['temp_max', 'temp_min', 'sunshine', 'precip'], freq='D')
 
-    # Cluster Time Series
-    series_clusters = cluster_series(target_series, k=config['clusters'], subset=config['subset'])
+    print(list(len(series) for series in timeseries))
+    timeseries = stack_timeseries(timeseries)
+    print(list(len(series) for series in timeseries))
 
-    if config['model'] in ['TFTModel', 'DeepAR', 'TCNModel']:
-        method = 'global'
-    else:
-        method = 'local'
+    # # custom selection of timeseries
+    # timeseries = [series for series in target_series if len(
+    #     series) == 1035]
+    # indices = [0, 1, 2, 4, 10, 11, 14, 15, 16, 17, 18, 20]
+    # timeseries = [timeseries[i] for i in indices][:args.subset]
+    
 
-    # Traing and Evaluate per cluster
+    if args.train_global:
+        timeseries = [concatenate(timeseries, axis=1, ignore_time_axis=True)]
+
+    # Traing and evaluate per timeseries
     predictions = []
-    for cluster in tqdm(series_clusters):
-        model = load_model(config)
+    for series in timeseries:
+        train, val, test = split_data(series, args.train_split, args.val_split)
+        covariates_train, covariates_val, covariates_test = split_data(
+            covariates, args.train_split, args.val_split)
 
-        if method == 'global':
-            forecast = train_predict_global(
-                model=model,
-                target_series=cluster,
-                covariates=covariates,
-                forecast_horizon=config['forecast_horizon'],
-                train_split=config['train_split'],
-                val_split=config['val_split'],
-                num_samples=config['num_samples'],
-                retrain=config['retrain'],)
+        if args.use_covariates:
+            # align covariates to target
+            covariates = covariates[series.time_index]
+
+        # scale data
+        if args.scale:
+            # scale training data
+            target_scaler = Scaler(MinMaxScaler())
+            train = target_scaler.fit_transform(train)
+            val = target_scaler.transform(val)
+            series = target_scaler.transform(series)
+
+            covariates_scaler = Scaler(MinMaxScaler())
+            covariates_train = covariates_scaler.fit_transform(
+                covariates_train)
+            covariates_val = covariates_scaler.transform(covariates_val)
+            covariates = covariates_scaler.transform(covariates)
+
+        # load model
+        model = load_model(args)
+
+        # train model on training data
+        training_args, eval_args = {}, {}
+        if args.use_covariates:
+            training_args['past_covariates'] = covariates, 
+            training_args['val_past_covariates'] = covariates
+            eval_args['past_covariates'] = covariates
+
+        if args.use_val:
+            training_args['val_series'] = val
+
+        # evaluate on all data
+        eval_args = {}
+        if args.retrain:
+            eval_args['retrain'] = True
         else:
-            forecast = train_predict_local(
-                model=model,
-                target_series=cluster,
-                covariates=covariates,
-                num_samples=config['num_samples'],
-                forecast_horizon=config['forecast_horizon'],
-                train_split=config['train_split'],
-                val_split=config['val_split'],
-            )
+            eval_args['retrain'] = False
 
-        predictions.append(forecast)
+        model.fit(
+            series=train,
+            **training_args
+        )
+        
+        backtest = model.historical_forecasts(
+            series=series,
+            num_samples=args.num_samples,
+            start=test.start_time(),
+            forecast_horizon=args.forecast_horizon,
+            verbose=False,
+            **eval_args,
+        )
 
-    print("Finished training")
+        prediction = target_scaler.inverse_transform(backtest)
+        predictions.append(prediction)
+
+    logging.info(f"Finished training")
     # Log Metrics
-    scores = evaluate(predictions, series_clusters)
+    scores = evaluate(predictions, timeseries)
     logging.info(f"results: {scores}")
 
     # Save Forecast plot
-    figs = plot_separate(predictions, series_clusters)
+    figs = plot_separate(predictions, timeseries)
     for location_id, fig in figs.items():
-        fig.savefig(config['logdir'] + "plots/" + f"{location_id}.png")
-    
+        fig.savefig(args.logdir + "plots/" + f"{location_id.replace('/', '_')}.png")
 
 
 if __name__ == "__main__":
-    import logging
 
-    logging.getLogger("lightning.pytorch.utilities.rank_zero").setLevel(logging.WARNING)
+    logging.getLogger("pytorch_lightning").setLevel(logging.WARNING)
 
     parser = argparse.ArgumentParser(description="Argument Parser")
 
     # Data options
-    parser.add_argument("--location_type", type=str, default='on_forecourt')
-    parser.add_argument("--clusters", type=int, default=None)
+    parser.add_argument("--train_global", action='store_true')
     parser.add_argument("--subset", type=int, default=None)
+    parser.add_argument("--use_covariates", action='store_true')
+    parser.add_argument("--use_val", action='store_true')
+    parser.add_argument("--scale", action='store_false')
 
     # Training options
-    parser.add_argument("--model", type=str, default="Naive",
-                        help="a string specifying the model (Naive, DeepAR, ARIMA)")
+    parser.add_argument("--model", type=str, default="NBEATS",
+                        help="a string specifying the model")
     parser.add_argument("--train_split", type=float, default=0.7)
     parser.add_argument("--val_split", type=float, default=0.8)
 
     # Inference options
-    parser.add_argument("--retrain", type=bool, default=False)
+    parser.add_argument("--retrain", action='store_true')
     parser.add_argument("--forecast_horizon", type=int, default=1)
-    parser.add_argument("--num_samples", type=int, default=25)
+    parser.add_argument("--num_samples", type=int, default=1)
 
-   
     args = parser.parse_args()
-    config = vars(args)
+    print(args)
 
     start_time_str = datetime.today().strftime('%d-%m-%Y_%H-%M-%S')
-    logdir = f"logs/{start_time_str}_{config['model']}_{config['clusters']}/"
-    os.mkdir(logdir)
-    os.mkdir(logdir + "plots/") 
+    args.logdir = f"logs/{start_time_str}_{args.model=}_{args.train_global=}_{args.use_covariates=}/"
+    os.mkdir(args.logdir)
+    os.mkdir(args.logdir + "plots/")
 
-    logging.basicConfig(filename=logdir + "logs.log", encoding='utf-8', level=logging.INFO)
+    logging.basicConfig(filename=args.logdir + "logs.log",
+                        encoding='utf-8', level=logging.INFO)
 
+    # # Load Data
+    # target_series = load_target(
+    #     path='data/03_processed/on_forecourt_sessions.csv',
+    #     group_cols='location_id',
+    #     time_col='date',
+    #     value_cols='energy_delivered_kwh',
+    #     static_cols=['num_evse'],
+    #     freq='D'
+    # )
+    # Load Data
+    target_series = load_target(
+        path='data/03_processed/palo_alto_dataset.csv',
+        group_cols='location_id',
+        time_col='date',
+        value_cols='energy_delivered_kwh',
+        # static_cols=['num_evse'],
+        freq='D'
+    )
 
-    config['logdir'] = logdir
+    covariates = load_covariates(
+        path='data/03_processed/weather_ecad.csv',
+        time_col='date',
+        value_cols=['temp_max', 'temp_min', 'sunshine', 'precip'],
+        freq='D'
+    )
 
-    logging.info(f"Running Experiments, params: {config}")
-    
-    run(config)
+    logging.info(f"Running Experiments, params: {args}")
+    run(target_series, covariates, args)
