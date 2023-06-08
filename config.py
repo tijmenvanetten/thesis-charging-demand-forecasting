aaed544450498180f764237dba8070e808bdae2c
tijmenvanetten <tijmenettenvan@gmail.com>
2023-06-08 13:55:19 +0200

    add config file

---
+++ config.py
@@ -0,0 +1,3 @@
+SHELL_DATASET_PATH = "C:\Users\tijmen.vanetten\Documents\emobility-vanetten\data\03_processed\shell_dataset.csv"
+PALO_ALTO_DATASET_PATH = "C:\Users\tijmen.vanetten\Documents\emobility-vanetten\data\03_processed\palo_alto_dataset.csv"
+BOULDER_DATASET_PATH = "C:\Users\tijmen.vanetten\Documents\emobility-vanetten\data\03_processed\boulder_dataset.csv"
